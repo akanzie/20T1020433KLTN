64dd3ce7a41d96a31c7b2d744c0b83b13c7c1199
akanzie <114471183+akanzie@users.noreply.github.com>
2024-04-12 17:35:33 +0700

    config

--- KLTN20T1020433.Web/Program.cs
+++ KLTN20T1020433.Web/Program.cs
@@ -1,7 +1,8 @@
 using KLTN20T1020433.Web.AppCodes;
+using KLTN20T1020433.Web.Configuration;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 builder.Services.AddControllersWithViews().AddMvcOptions(option =>
 {
     option.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
@@ -14,22 +15,19 @@ builder.Services.AddSession(option =>
     option.Cookie.IsEssential = true;
 });
 builder.Services.AddHttpContextAccessor();
-
+DatabaseConfig.Initialize(builder.Configuration.GetConnectionString("SQLServerConnectionString"));
+FileConfig.Initialize(builder.Configuration.GetSection(FileConfig.FILE_STORAGE_PATHS)["ServerStoragePath"]);
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
@@ -40,10 +38,5 @@ ApplicationContext.Configure
     httpContextAccessor: app.Services.GetRequiredService<IHttpContextAccessor>(),
     hostEnvironment: app.Services.GetService<IWebHostEnvironment>()
 );
-
-string connectionString = @"server=LAPCN-KietCA;user id=sa;password=1;database=QuanLyBaiThi;TrustServerCertificate=true";
-string fileStoragePath = @"D:\File";
-KLTN20T1020433.BusinessLayers.Configuration.Initialize(connectionString);
-KLTN20T1020433.BusinessLayers.Configuration.GetFileStoragePath(fileStoragePath);
 app.Run();
 
