79a114437c4ac79f348723a402ec7451e9f5a428
akanzie <114471183+akanzie@users.noreply.github.com>
2024-05-18 20:09:52 +0700

    feat: complete

---
+++ KLTN20T1020433.Web/Areas/Teacher/Controllers/SubmissionController.cs
@@ -0,0 +1,193 @@
+﻿using AutoMapper;
+using KLTN20T1020433.Application.Queries.TeacherQueries;
+using KLTN20T1020433.Application.Services;
+using KLTN20T1020433.Web.AppCodes;
+using KLTN20T1020433.Web.Areas.Teacher.Models;
+using KLTN20T1020433.Web.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KLTN20T1020433.Web.Areas.Teacher.Controllers
+{
+    [Area("Teacher")]
+    [Authorize]
+    [TeacherOnlyFilter]
+    public class SubmissionController : Controller
+    {
+        const int TEST_PAGE_SIZE = 10;
+        const int SUBMISSION_PAGE_SIZE = 40;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+        public SubmissionController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Search(GetSubmissionsBySearchQuery input, bool searchInDetail = true)
+        {
+            try
+            {
+                var user = User.GetUserData();
+                var test = await _mediator.Send(new GetTestDetailQuery { Id = input.TestId, TeacherId = user.UserId });
+                if (test == null)
+                {
+                    return View("NotFound");
+                }
+                int rowCount = await _mediator.Send(new GetRowCountSubmissionsQuery { SearchValue = input.SearchValue, Statuses = input.Statuses, TestId = input.TestId });
+
+                var data = await _mediator.Send(input);
+
+                var model = new SubmissonSearchResult()
+                {
+                    Page = input.Page,
+                    PageSize = input.PageSize,
+                    SearchValue = input.SearchValue ?? "",
+                    TestId = input.TestId,
+                    Statuses = input.Statuses ?? "",
+                    RowCount = rowCount,
+                    Data = data
+                };
+                ApplicationContext.SetSessionData(Constants.SUBMISSION_SEARCH, input);
+                return searchInDetail ? View(model) : View("SearchStudent", model);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred in SearchSubmission: {ex.Message}");
+                return Json(ErrorMessages.RequestNotCompleted);
+            }
+        }
+        public async Task<IActionResult> Detail(int id = 0, int testId = 0)
+        {
+            try
+            {
+                var user = User.GetUserData();
+                if (id <= 0 || testId <= 0)
+                    return View("NotFound");
+                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
+                if (test == null)
+                {
+                    return View("NotFound");
+                }
+                var submission = await _mediator.Send(new GetSubmissionByIdQuery { Id = id });
+                if (submission == null)
+                    return View("NotFound");
+                var files = await _mediator.Send(new GetFilesBySubmissionIdQuery { SubmissionId = id, Status = submission.Status });
+                if (files == null || !files.Any())
+                {
+                    return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.FileNotFound });
+                }
+                var input = ApplicationContext.GetSessionData<GetSubmissionsBySearchQuery>(Constants.STUDENT_SEARCH);
+                if (input == null)
+                {
+                    input = new GetSubmissionsBySearchQuery()
+                    {
+                        Page = 1,
+                        PageSize = SUBMISSION_PAGE_SIZE,
+                        SearchValue = "",
+                        TestId = testId,
+                        Statuses = ""
+                    };
+                }
+                var model = new SubmissionModel
+                {
+                    TestId = test.TestId,
+                    Title = test.Title,
+                    Files = files,
+                    Submission = submission,
+                    SearchQuery = input
+
+                }; ApplicationContext.SetSessionData(Constants.SUBMISSION_SEARCH, input);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred in SubmissionDetail: {ex.Message}");
+                return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.RequestNotCompleted });
+            }
+        }
+        public async Task<IActionResult> File(Guid fileId, int testId = 0, int submissionId = 0)
+        {
+            try
+            {
+                var user = User.GetUserData();
+                if (testId <= 0)
+                {
+                    return View("NotFound");
+                }
+                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
+                if (test == null)
+                {
+                    return View("NotFound");
+                }
+                var submission = await _mediator.Send(new GetSubmissionByIdQuery { Id = submissionId });
+                if (submission == null)
+                    return View("NotFound");
+                var file = await _mediator.Send(new GetSubmissionFileByIdQuery { Id = fileId });
+                if (file == null)
+                {
+                    return View("NotFound");
+                }
+                var fileViewModel = new FileViewModel
+                {
+                    FileId = file.FileId,
+                    FileName = file.OriginalName,
+                    FilePath = file.FilePath,
+                    MimeType = file.MimeType,
+                    Content = file.MimeType.StartsWith("text") || file.MimeType.Contains("word")
+                        ? FileUtils.ConvertToHtmlAsync(file.FilePath)
+                        : null,
+                    IsImage = file.MimeType.StartsWith("image"),
+                    IsText = file.MimeType.StartsWith("text") || file.MimeType.Contains("word"),
+                    TestId = testId
+                };
+
+                return View(fileViewModel);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred in SubmissionFile: {ex.Message}");
+                return Json(ErrorMessages.RequestNotCompleted);
+            }
+        }
+        public async Task<IActionResult> CountSubmission(GetRowCountSubmissionsQuery input)
+        {
+            try
+            {
+                int count = await _mediator.Send(new GetRowCountSubmissionsQuery { SearchValue = input.SearchValue, Statuses = input.Statuses, TestId = input.TestId });
+                return Json(new { success = true, count = count });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred in CountSubmission: {ex.Message}");
+                return Json(new { success = false, count = 0 });
+            }
+        }
+        public async Task<IActionResult> DownloadAllSubmissionFile(int testId)
+        {
+            try
+            {
+                var user = User.GetUserData();
+                if (testId <= 0)
+                {
+                    return View("NotFound");
+                }
+                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
+                if (test == null)
+                    return View("NotFound");
+                string zipName = $"{test.Title.RemoveDiacritics().Trim()}.zip";
+                var submissionFiles = await _mediator.Send(new GetSubmissionFilesByTestIdQuery { TestId = testId });
+                var archiveStream = FileUtils.ZipFiles(submissionFiles);
+                return File(archiveStream, "application/zip", zipName);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred in DownloadAllSubmissionFile: {ex.Message}");
+                return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.RequestNotCompleted });
+            }
+        }
+    }
+}
--- KLTN20T1020433.Web/Areas/Teacher/Controllers/TestController.cs
+++ KLTN20T1020433.Web/Areas/Teacher/Controllers/TestController.cs
@@ -2,13 +2,10 @@
 using KLTN20T1020433.Application.Commands.TeacherCommands.Create;
 using KLTN20T1020433.Application.Commands.TeacherCommands.Delete;
 using KLTN20T1020433.Application.Commands.TeacherCommands.Update;
-using KLTN20T1020433.Application.DTOs;
 using KLTN20T1020433.Application.DTOs.TeacherDTOs;
 using KLTN20T1020433.Application.Queries;
 using KLTN20T1020433.Application.Queries.TeacherQueries;
 using KLTN20T1020433.Application.Services;
-using KLTN20T1020433.Domain.Student;
-using KLTN20T1020433.Domain.Submission;
 using KLTN20T1020433.Domain.Test;
 using KLTN20T1020433.Web.AppCodes;
 using KLTN20T1020433.Web.Areas.Teacher.Commands.Delete;
@@ -17,8 +14,6 @@ using KLTN20T1020433.Web.Models;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Buffers;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace KLTN20T1020433.Web.Controllers.Teacher
 {
@@ -80,40 +75,7 @@ namespace KLTN20T1020433.Web.Controllers.Teacher
             }
 
         }
-        public async Task<IActionResult> SearchSubmission(GetSubmissionsBySearchQuery input, bool searchInDetail = true)
-        {
-            try
-            {
-                var user = User.GetUserData();
-                var test = await _mediator.Send(new GetTestDetailQuery { Id = input.TestId, TeacherId = user.UserId });
-                if (test == null)
-                {
-                    return View("NotFound");
-                }
-                int rowCount = await _mediator.Send(new GetRowCountSubmissionsQuery { SearchValue = input.SearchValue, Statuses = input.Statuses, TestId = input.TestId });
-
-                var data = await _mediator.Send(input);
-
-                var model = new SubmissonSearchResult()
-                {
-                    Page = input.Page,
-                    PageSize = input.PageSize,
-                    SearchValue = input.SearchValue ?? "",
-                    TestId = input.TestId,
-                    Statuses = input.Statuses ?? "",
-                    RowCount = rowCount,
-                    Data = data
-                };
-                ApplicationContext.SetSessionData(Constants.SUBMISSION_SEARCH, input);
-                return searchInDetail ? View(model) : View("SearchStudentInSubmission", model);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred in SearchSubmission: {ex.Message}");
-                return Json(ErrorMessages.RequestNotCompleted);
-            }
-        }
+        
         [HttpPost]
         public async Task<IActionResult> CancelCreation(int testId = 0)
         {
@@ -259,116 +221,7 @@ namespace KLTN20T1020433.Web.Controllers.Teacher
                 return Json(ErrorMessages.RequestNotCompleted);
             }
         }
-        public IActionResult SelectCourses()
-        {
-            return View();
-        }
-        public async Task<IActionResult> Submission(int id = 0, int testId = 0)
-        {
-            try
-            {
-                var user = User.GetUserData();
-                if (id <= 0 || testId <= 0)
-                    return View("NotFound");
-                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
-                if (test == null)
-                {
-                    return View("NotFound");
-                }
-                var submission = await _mediator.Send(new GetSubmissionByIdQuery { Id = id });
-                if (submission == null)
-                    return View("NotFound");
-                var files = await _mediator.Send(new GetFilesBySubmissionIdQuery { SubmissionId = id, Status = submission.Status });
-                if (files == null || !files.Any())
-                {
-                    return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.FileNotFound });
-                }
-                var input = ApplicationContext.GetSessionData<GetSubmissionsBySearchQuery>(Constants.STUDENT_SEARCH);
-                if (input == null)
-                {
-                    input = new GetSubmissionsBySearchQuery()
-                    {
-                        Page = 1,
-                        PageSize = SUBMISSION_PAGE_SIZE,
-                        SearchValue = "",
-                        TestId = testId,
-                        Statuses = ""
-                    };
-                }
-                var model = new SubmissionModel
-                {
-                    TestId = test.TestId,
-                    Title = test.Title,
-                    Files = files,
-                    Submission = submission,
-                    SearchQuery = input
-
-                }; ApplicationContext.SetSessionData(Constants.SUBMISSION_SEARCH, input);
-                return View(model);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred in RemoveTestFile: {ex.Message}");
-                return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.RequestNotCompleted });
-            }
-        }
-        public async Task<IActionResult> SubmissionFile(Guid fileId, int testId = 0, int submissionId = 0)
-        {
-            try
-            {
-                var user = User.GetUserData();
-                if (testId <= 0)
-                {
-                    return View("NotFound");
-                }
-                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
-                if (test == null)
-                {
-                    return View("NotFound");
-                }
-                var submission = await _mediator.Send(new GetSubmissionByIdQuery { Id = submissionId });
-                if (submission == null)
-                    return View("NotFound");
-                var file = await _mediator.Send(new GetSubmissionFileByIdQuery { Id = fileId });
-                if (file == null)
-                {
-                    return View("NotFound");
-                }
-                var fileViewModel = new FileViewModel
-                {
-                    FileId = file.FileId,
-                    FileName = file.OriginalName,
-                    FilePath = file.FilePath,
-                    MimeType = file.MimeType,
-                    Content = file.MimeType.StartsWith("text") || file.MimeType.Contains("word")
-                        ? FileUtils.ConvertToHtmlAsync(file.FilePath)
-                        : null,
-                    IsImage = file.MimeType.StartsWith("image"),
-                    IsText = file.MimeType.StartsWith("text") || file.MimeType.Contains("word"),
-                    TestId = testId
-                };
-
-                return View(fileViewModel);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred in SubmissionFile: {ex.Message}");
-                return Json(ErrorMessages.RequestNotCompleted);
-            }
-        }
-        public async Task<IActionResult> CountSubmission(GetRowCountSubmissionsQuery input)
-        {
-            try
-            {
-                int count = await _mediator.Send(new GetRowCountSubmissionsQuery { SearchValue = input.SearchValue, Statuses = input.Statuses, TestId = input.TestId });
-                return Json(new { success = true, count = count });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occurred in Edit: {ex.Message}");
-                return Json(new { success = false, count = 0 });
-            }
-        }
+        
         public async Task<IActionResult> Edit(int id = 0)
         {
             try
@@ -494,8 +347,6 @@ namespace KLTN20T1020433.Web.Controllers.Teacher
                 }
                 byte[] fileBytes = await FileUtils.ReadFileAsync(filePath);
                 return File(fileBytes, mimeType, file.OriginalName);
-
-
             }
             catch (Exception ex)
             {
@@ -503,30 +354,7 @@ namespace KLTN20T1020433.Web.Controllers.Teacher
                 return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.RequestNotCompleted });
             }
         }
-        public async Task<IActionResult> DownloadAllSubmissionFile(int testId)
-        {
-            try
-            {
-                var user = User.GetUserData();
-                if (testId <= 0)
-                {
-                    return View("NotFound");
-                }
-                var test = await _mediator.Send(new GetTestByIdQuery { Id = testId, TeacherId = user.UserId });
-                if (test == null)
-                    return View("NotFound");
-                string zipName = $"{test.Title.RemoveDiacritics().Trim()}.zip";
-                var submissionFiles = await _mediator.Send(new GetSubmissionFilesByTestIdQuery { TestId = testId });
-                var archiveStream = FileUtils.ZipFiles(submissionFiles);
-                return File(archiveStream, "application/zip", zipName);
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An exception occurred: {ex.Message}");
-                return View("Error", new ErrorMessageModel { Title = "Đã xảy ra lỗi không mong muốn", Content = ErrorMessages.RequestNotCompleted });
-            }
-        }
+        
 
         [HttpPost]
         public async Task<IActionResult> UploadTestFile(List<IFormFile> files, int? testId = 0)
